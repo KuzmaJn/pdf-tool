88e6ce8eb90aec0e053f563a7bc46b5b24b4f307
Marek Kormoš <lonzikormos@gmail.com>
2025-05-17 19:09:53 +0200

    WIP changes for Janci

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PdfApiController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::post('pdf/merge', [PdfApiController::class, 'merge'])->name('pdf.merge');
+Route::get('/download/{filename}', [\App\Http\Controllers\PdfApiController::class, 'download'])->name('pdf.download');
+
